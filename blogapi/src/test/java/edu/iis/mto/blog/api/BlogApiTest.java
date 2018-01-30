deaeec55c7e3de3645ededc70facfb365c180fc9
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2018-01-30 20:03:23 +0100

    (3.3) Dodanie testu throwingDataIntegrityViolationExceptionShouldGenerate409Status()

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,32 +27,46 @@ import edu.iis.mto.blog.services.DataFinder;
 @WebMvcTest(BlogApi.class)
 public class BlogApiTest {
 
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-    @MockBean
-    private BlogService blogService;
+	@MockBean
+	private BlogService blogService;
 
-    @MockBean
-    private DataFinder finder;
+	@MockBean
+	private DataFinder finder;
 
-    @Test
-    public void postBlogUserShouldResponseWithStatusCreatedAndNewUserId() throws Exception {
-        Long newUserId = 1L;
-        UserRequest user = new UserRequest();
-        user.setEmail("john@domain.com");
-        user.setFirstName("John");
-        user.setLastName("Steward");
-        Mockito.when(blogService.createUser(user)).thenReturn(newUserId);
-        String content = writeJson(user);
+	@Test
+	public void postBlogUserShouldResponseWithStatusCreatedAndNewUserId() throws Exception {
+		Long newUserId = 1L;
+		UserRequest user = new UserRequest();
+		user.setEmail("john@domain.com");
+		user.setFirstName("John");
+		user.setLastName("Steward");
+		Mockito.when(blogService.createUser(user)).thenReturn(newUserId);
+		String content = writeJson(user);
 
-        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaType.APPLICATION_JSON_UTF8).content(content)).andExpect(status().isCreated())
-                .andExpect(content().string(writeJson(new Id(newUserId))));
-    }
+		mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaType.APPLICATION_JSON_UTF8).content(content)).andExpect(status().isCreated())
+				.andExpect(content().string(writeJson(new Id(newUserId))));
+	}
 
-    private String writeJson(Object obj) throws JsonProcessingException {
-        return new ObjectMapper().writer().writeValueAsString(obj);
-    }
+	@Test
+	public void throwingDataIntegrityViolationExceptionShouldGenerate409Status() throws Exception {
+
+		UserRequest user = new UserRequest();
+		user.setEmail("john@domain.com");
+		user.setFirstName("John");
+		user.setLastName("Steward");
+		Mockito.when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+		String content = writeJson(user);
+
+		mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaType.APPLICATION_JSON_UTF8).content(content)).andExpect(status().isConflict());
+	}
+
+	private String writeJson(Object obj) throws JsonProcessingException {
+		return new ObjectMapper().writer().writeValueAsString(obj);
+	}
 
 }
