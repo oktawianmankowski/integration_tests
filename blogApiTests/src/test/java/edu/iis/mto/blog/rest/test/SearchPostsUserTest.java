b3b73e87cb119fdf035ce50600d2a7ffb6917198
198176 <adriantomczak36@gmail.com>
2018-02-04 11:22:07 +0100

    3.4.4 Test searchingPostsShouldReturnLikesCountEqualsOne

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostsUserTest.java
@@ -0,0 +1,33 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SearchPostsUserTest {
+
+    @BeforeClass
+    public static void setUp() {
+        JSONObject jsonPost = new JSONObject().put("entry", "post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonPost.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/1/post");
+    }
+
+    @Test
+    public void searchingPostsShouldReturnLikesCountEqualsOne() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK).when().post("/blog/user/3/like/1");
+        int likesCount = RestAssured.given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8").expect().log().all()
+                .statusCode(HttpStatus.SC_OK).when().get("/blog/user/1/post").then().extract().jsonPath()
+                .getInt("likesCount[0]");
+        Assert.assertThat(likesCount, Matchers.is(1));
+    }
+
+}
