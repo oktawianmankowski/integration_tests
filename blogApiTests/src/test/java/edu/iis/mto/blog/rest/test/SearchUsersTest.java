9a56572629bea33648b5613f098769ae69e4d4e5
198176 <adriantomczak36@gmail.com>
2018-02-04 18:42:18 +0100

    3.4.5 Test searchingUsersByStringShouldReturnWithoutUserWithAccountStatusRemoved

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUsersTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUsersTest.java
@@ -17,12 +17,21 @@ public class SearchUsersTest {
     }
 
     @Test
-    public void searchingUserByStringWithAccountStatusRemovedShouldBeEmpty() {
+    public void searchingUsersByStringWithOnlyAccountStatusRemovedShouldBeEmpty() {
         String userCount = RestAssured.given().accept(ContentType.JSON)
                 .header("Content-Type", "application/json;charset=UTF-8").expect().log().all()
                 .statusCode(HttpStatus.SC_OK).when().get("/blog/user/find?searchString=Delete")
                 .then().extract().asString();
         Assert.assertThat(userCount, Matchers.is("[]"));
+    }
+
+    @Test
+    public void searchingUsersByStringShouldReturnWithoutUserWithAccountStatusRemoved() {
+        int userCount = RestAssured.given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8").expect().log().all()
+                .statusCode(HttpStatus.SC_OK).when().get("/blog/user/find?searchString=John").then().extract()
+                .jsonPath().getList("$").size();
+        Assert.assertThat(userCount, Matchers.is(1));
     }
 
 }
