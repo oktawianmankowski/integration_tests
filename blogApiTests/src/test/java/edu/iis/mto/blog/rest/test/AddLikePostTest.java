5827e6f91c384e057218d12f57cdb47d954c1171
198176 <adriantomczak36@gmail.com>
2018-02-04 10:41:19 +0100

    3.4.3 Test addingLikeToTheSamePostShouldNotChangeItsStatus

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
@@ -1,8 +1,10 @@
 package edu.iis.mto.blog.rest.test;
 
 import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.json.JSONObject;
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
@@ -10,8 +12,8 @@ import com.jayway.restassured.http.ContentType;
 
 public class AddLikePostTest {
 
-    @Before
-    public  void setUp() {
+    @BeforeClass
+    public static void setUp() {
         JSONObject jsonPost = new JSONObject().put("entry", "post");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonPost.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
@@ -34,6 +36,19 @@ public class AddLikePostTest {
     public void notCanAddingLikeToPostByUserWithStatusToNew() {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when().post("/blog/user/2/like/1");
+    }
+
+    @Test
+    public void addingLikeToTheSamePostShouldNotChangeItsStatus() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK).when().post("/blog/user/3/like/1");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK).when().post("/blog/user/3/like/1");
+        int likesCount = RestAssured.given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8").expect().log().all()
+                .statusCode(HttpStatus.SC_OK).when().get("/blog/user/1/post").then().extract().jsonPath()
+                .getInt("likesCount[0]");
+        Assert.assertThat(likesCount, Matchers.is(1));
     }
 
 }
