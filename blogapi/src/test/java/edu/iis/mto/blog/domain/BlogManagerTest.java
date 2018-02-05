4d9a23d8e8ec6297e3f926e52a0bd336a97400af
smoczynskaj <smoczynska.justyna@gmail.com>
2018-02-05 18:42:03 +0100

    3.2 Sprawdzenie czy zostanie rzucony wyjątek dla nowego użytkownika

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,5 +1,16 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
+import edu.iis.mto.blog.domain.repository.UserRepository;
+import edu.iis.mto.blog.mapper.DataMapper;
+import edu.iis.mto.blog.services.BlogService;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,19 +22,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import edu.iis.mto.blog.api.request.UserRequest;
-import edu.iis.mto.blog.domain.model.AccountStatus;
-import edu.iis.mto.blog.domain.model.User;
-import edu.iis.mto.blog.domain.repository.UserRepository;
-import edu.iis.mto.blog.mapper.DataMapper;
-import edu.iis.mto.blog.services.BlogService;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     UserRepository userRepository;
+
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
 
     @Autowired
     DataMapper dataMapper;
@@ -38,6 +48,53 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test (expected = DomainError.class)
+    public void shouldThrowErrorForNewUser() {
+        User user = createUser();
+        User user2 = createUser2();
+
+        Mockito.when(userRepository.findOne(1L)).thenReturn(user);
+        Mockito.when(userRepository.findOne(2L)).thenReturn(user2);
+        Mockito.when(blogPostRepository.findOne(1L)).thenReturn(createBlogPost(user));
+
+        blogService.addLikeToPost(2L, 1L);
+    }
+
+    private User createUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("john_d@domain.com");
+        user.setAccountStatus(AccountStatus.CONFIRMED);
+        return user;
+    }
+
+    private User createUser2() {
+        User user = new User();
+        user.setId(2L);
+        user.setFirstName("Tom");
+        user.setLastName("Jones");
+        user.setEmail("t_jones@domain.com");
+        user.setAccountStatus(AccountStatus.NEW);
+        return user;
+    }
+
+    private BlogPost createBlogPost(User user) {
+        BlogPost blogPost = new BlogPost();
+        blogPost.setId(1L);
+        blogPost.setUser(user);
+        blogPost.setEntry("Post 1");
+        return blogPost;
+    }
+
+    private LikePost createLikePost(User user, BlogPost post) {
+        LikePost likePost = new LikePost();
+        likePost.setUser(user);
+        likePost.setPost(post);
+        return likePost;
     }
 
 }
