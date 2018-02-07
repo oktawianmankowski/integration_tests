3c71831e58c61fee775d6885b0ca8a8db904ead7
smoczynskaj <smoczynska.justyna@gmail.com>
2018-02-07 18:38:36 +0100

    3.4.2 Sprawdzanie dla próby dodania like'a dla własnego posta

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -41,4 +41,27 @@ public class CreateUserTest extends FunctionalTests {
                 .post("/blog/user/" + userId + "/post").body();
 
     }
+
+    @Test
+    public void shouldNotAllowToLikeOwnPost() {
+        JSONObject userObj = new JSONObject().put("email", "tracy5@domain.com");
+        JSONObject postObj = new JSONObject().put("entry", "Post content");
+        ResponseBody createUserResponse = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(userObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user").body();
+
+        Integer userId = createUserResponse.jsonPath().getJsonObject("id");
+
+        ResponseBody createPostResponse = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(postObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/" + userId + "/post").body();
+
+        Integer postId = createPostResponse.jsonPath().getJsonObject("id");
+
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(postObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/" + userId + "/like/" + postId);
+    }
+
+
 }
