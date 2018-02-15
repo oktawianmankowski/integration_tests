b25dd1b470163dc625c7b6606ffe7e868d32bbea
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2018-02-15 23:38:11 +0100

    [3.4.4] Test czy wyszukiwanie postów zwraca własciwą liczbę polubien

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
@@ -5,11 +5,14 @@ import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.ResponseBody;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class FunctionalTests {
 
@@ -105,10 +108,29 @@ public class FunctionalTests {
     }
 
     @Test
-    public void searchPostOfDeleteUser(){
+    public void searchPostOfDeleteUserShouldReturnNotFoundResponse(){
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_NOT_FOUND).when()
                 .get("/blog/user/" + 18 + "/post");
+
+    }
+
+    @Test
+    public void searchPostOfUserShouldReturnCorrectLikeCountForpost(){
+        int expectedLikescount = 1;
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .post("/blog/user/" + 2 + "/like/" + 3);
+
+        ResponseBody responseBody = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .get("/blog/user/" + 4 + "/post").getBody();
+
+        JSONArray jsonArray = new JSONArray(responseBody.print());
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        int likesCount = (int)jsonObject.get("likesCount");
+
+        Assert.assertThat(likesCount, is(expectedLikescount));
 
     }
 
