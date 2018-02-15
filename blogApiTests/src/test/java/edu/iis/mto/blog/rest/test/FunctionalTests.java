bc677ff51b6280e773f7bd0139b151799b7b8d9f
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2018-02-15 22:24:58 +0100

    [3.4.2] Testy dodania posta

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
@@ -49,4 +49,20 @@ public class FunctionalTests {
                 .post("/blog/user");
     }
 
+    @Test
+    public void AddPostByConfirmedUserShouldReturnCreateStatus() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/" + 1 + "/post");
+    }
+
+    @Test
+    public void AddPostByNewUserShouldReturnBadRequestStatus() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/" + 2 + "/post");
+    }
+
 }
