b99ce460f0b5ddcc141176c6a1a5b8ecb7bc600e
jankowski.lukasz <191867@edu.p.lodz.pl>
2018-02-18 00:44:11 +0100

    3.2 dodanie testu onlyUsersAfterConfirmationCanLikePosts

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,5 +1,9 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,6 +22,8 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -31,6 +37,12 @@ public class BlogManagerTest {
     @Autowired
     BlogService blogService;
 
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
+
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
@@ -38,6 +50,35 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test
+    public void onlyUsersAfterConfirmationCanLikePosts() {
+        User firstUser = new User();
+        firstUser.setId(7L);
+        firstUser.setFirstName("john");
+        firstUser.setLastName("doe");
+        firstUser.setEmail("john@doe.com");
+        firstUser.setAccountStatus(AccountStatus.NEW); // can't like posts
+
+        User secondUser = new User();
+        secondUser.setId(17L);
+        secondUser.setFirstName("Tom");
+        secondUser.setLastName("Brown");
+        secondUser.setEmail("tom@brown.com");
+        secondUser.setAccountStatus(AccountStatus.CONFIRMED); // can like posts
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(firstUser);
+        blogPost.setEntry("Entry");
+        blogPost.setId(717L);
+
+        Mockito.when(userRepository.findOne(secondUser.getId())).thenReturn(secondUser);
+        Mockito.when(blogPostRepository.findOne(blogPost.getId())).thenReturn(blogPost);
+        Optional<LikePost> likePost = Optional.empty();
+        Mockito.when(likePostRepository.findByUserAndPost(secondUser, blogPost)).thenReturn(likePost);
+
+        Assert.assertThat(blogService.addLikeToPost(secondUser.getId(), blogPost.getId()), Matchers.is(true));
     }
 
 }
