193ce1fdeb4a4278d42652a4480a15cc29f9b42f
Rafał <fanghon@gmail.com>
2018-02-17 14:56:43 +0100

    tests to verify adding likes to posts

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -0,0 +1,46 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class LikePostTest extends FunctionalTests {
+
+	@Test
+	public void confirmedUserCanLikePost() {
+		JSONObject jsonObject = new JSONObject().put("entry", "Entry");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObject.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+				.post("/blog/user/1/like/2");
+	}
+
+	@Test
+	public void unconfirmedUserCannotNotLikePost() {
+		JSONObject jsonObject = new JSONObject().put("entry", "Entry");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObject.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+				.post("/blog/user/2/like/1");
+	}
+
+	@Test
+	public void userCannotLikeOwnPost() {
+		JSONObject jsonObject = new JSONObject().put("entry", "Entry");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObject.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+				.post("/blog/user/1/like/1");
+	}
+	
+	@Test
+	public void likingLikedPostDoesNotChangeStatus() {
+		JSONObject jsonObject = new JSONObject().put("entry", "Entry");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObject.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+				.post("/blog/user/1/like/2");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+		.body(jsonObject.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+		.post("/blog/user/1/like/2");
+	}
+}
