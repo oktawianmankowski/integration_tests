7e182eb66bdb0c41715534d9163bc1976e88c46e
Rafał <fanghon@gmail.com>
2018-02-17 12:44:17 +0100

    Additional tests for finding user in repository

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.blog.domain.repository;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 import org.hamcrest.Matchers;
@@ -19,6 +21,10 @@ import edu.iis.mto.blog.domain.model.User;
 @DataJpaTest
 public class UserRepositoryTest {
 
+	private static final String FIRST_NAME = "Jan";
+	private static final String LAST_NAME = "Kowalski";
+	private static final String EMAIL = "jan@kowalski.com";
+
 	@Autowired
 	private TestEntityManager entityManager;
 
@@ -30,8 +36,9 @@ public class UserRepositoryTest {
 	@Before
 	public void setUp() {
 		user = new User();
-		user.setFirstName("Jan");
-		user.setEmail("john@domain.com");
+		user.setFirstName(FIRST_NAME);
+		user.setLastName(LAST_NAME);
+		user.setEmail(EMAIL);
 		user.setAccountStatus(AccountStatus.NEW);
 		repository.deleteAllInBatch();
 	}
@@ -59,6 +66,45 @@ public class UserRepositoryTest {
 		User persistedUser = repository.save(user);
 
 		Assert.assertThat(persistedUser.getId(), Matchers.notNullValue());
+	}
+
+	@Test
+	public void shouldFindUserByCorrectFirstName() {
+		repository.save(user);
+		List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+				FIRST_NAME, LAST_NAME, EMAIL);
+
+		assertEquals(1, users.size());
+		assertEquals(FIRST_NAME, users.get(0).getFirstName());
+	}
+
+	@Test
+	public void shouldFindUserByCorrectLastName() {
+		repository.save(user);
+		List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+				FIRST_NAME, LAST_NAME, EMAIL);
+
+		assertEquals(1, users.size());
+		assertEquals(LAST_NAME, users.get(0).getLastName());
+	}
+
+	@Test
+	public void shouldFindUserByCorrectEmail() {
+		repository.save(user);
+		List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+				FIRST_NAME, LAST_NAME, EMAIL);
+
+		assertEquals(1, users.size());
+		assertEquals(EMAIL, users.get(0).getEmail());
+	}
+
+	@Test
+	public void shouldNotFindIncorrectUser() {
+		repository.save(user);
+		List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+				"Adam", "Nowak", "adam@nowak.com");
+
+		assertEquals(0, users.size());
 	}
 
 }
