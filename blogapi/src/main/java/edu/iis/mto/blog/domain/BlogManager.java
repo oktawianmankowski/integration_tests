8becc99a8807e21a2a81b848df8a44d2cda43126
Rafał <fanghon@gmail.com>
2018-02-17 14:49:52 +0100

    fixed status code for unconfirmed user

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogManager.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogManager.java
@@ -30,6 +30,9 @@ public class BlogManager extends DomainService implements BlogService {
 	@Override
 	public Long createPost(Long userId, PostRequest postRequest) {
 		User user = userRepository.findOne(userId);
+		if (user.getAccountStatus() != AccountStatus.CONFIRMED) {
+			throw new DomainError("cannot create post without comnfirmed account");
+		}
 		BlogPost post = mapper.mapToEntity(postRequest);
 		post.setUser(user);
 		blogPostRepository.save(post);
