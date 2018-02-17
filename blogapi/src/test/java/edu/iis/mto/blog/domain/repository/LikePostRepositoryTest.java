1fb8de0bcfea43639acdfa5941a4ac86f7761494
Rafał <fanghon@gmail.com>
2018-02-17 13:14:14 +0100

    additional LikePostRepository tests

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -1,9 +1,13 @@
 package edu.iis.mto.blog.domain.repository;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,7 +29,10 @@ public class LikePostRepositoryTest {
 	private UserRepository userRepository;
 
 	@Autowired
-	private LikePostRepository postRepository;
+	private BlogPostRepository blogRepository;
+
+	@Autowired
+	private LikePostRepository likeRepository;
 
 	private static final String FIRST_NAME = "Jan";
 
@@ -60,11 +67,64 @@ public class LikePostRepositoryTest {
 		likePost.setPost(blogPost);
 
 		userRepository.deleteAllInBatch();
+		blogRepository.deleteAllInBatch();
+		likeRepository.deleteAllInBatch();
 	}
 
 	@Test
 	public void shouldNotFindPostsIfRepositoryIsEmpty() {
-		assertEquals(0, postRepository.findAll().size());
+		assertEquals(0, likeRepository.findAll().size());
+	}
+
+	@Test
+	public void shouldReturnPostByCorrectUserAndBlogPost() {
+		userRepository.save(user);
+		blogRepository.save(blogPost);
+		likeRepository.save(likePost);
+
+		Optional<LikePost> postOpt = likeRepository.findByUserAndPost(user, blogPost);
+		assertTrue(postOpt.isPresent());
+		assertEquals(likePost, postOpt.get());
+	}
+
+	@Test
+	public void shouldStoreANewPost() {
+		userRepository.save(user);
+		blogRepository.save(blogPost);
+		LikePost persistedPost = likeRepository.save(likePost);
+		assertNotNull(persistedPost.getId());
+	}
+
+	@Test
+	public void shouldNotReturnPostByIncorrectUser() {
+		userRepository.save(user);
+		blogRepository.save(blogPost);
+		likeRepository.save(likePost);
+
+		User otherUser = new User();
+		otherUser.setFirstName("Adam");
+		otherUser.setLastName("Nowak");
+		otherUser.setEmail("adam@nowak.com");
+		otherUser.setAccountStatus(AccountStatus.NEW);
+		userRepository.save(otherUser);
+
+		Optional<LikePost> postOpt = likeRepository.findByUserAndPost(otherUser, blogPost);
+		assertFalse(postOpt.isPresent());
+	}
+
+	@Test
+	public void shouldNotReturnPostByIncorrectPost() {
+		userRepository.save(user);
+		blogRepository.save(blogPost);
+		likeRepository.save(likePost);
+
+		BlogPost incorrectBlogPost = new BlogPost();
+		incorrectBlogPost.setUser(user);
+		incorrectBlogPost.setEntry("Incorrect Post");
+		blogRepository.save(incorrectBlogPost);
+
+		Optional<LikePost> postOpt = likeRepository.findByUserAndPost(user, incorrectBlogPost);
+		assertFalse(postOpt.isPresent());
 	}
 
 }
