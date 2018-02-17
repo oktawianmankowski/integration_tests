6fe0ee251a05653ea9052374c3795e076c172d9a
Rafał <fanghon@gmail.com>
2018-02-17 15:50:48 +0100

    Searching user posts tests

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.User;
 import edu.iis.mto.blog.dto.PostData;
@@ -45,6 +47,9 @@ public class BlogDataFinder extends DomainService implements DataFinder {
 	@Override
 	public List<PostData> getUserPosts(Long userId) {
 		User user = userRepository.findOne(userId);
+		if (user.getAccountStatus().equals(AccountStatus.REMOVED)) {
+			throw new DomainError("Account has been removed");
+		}
 		List<BlogPost> posts = blogPostRepository.findByUser(user);
 		return posts.stream().map(post -> mapper.mapToDto(post)).collect(Collectors.toList());
 	}
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,5 @@
---this script initiates db for h2 db (used in test profile)
+--this script initiates db for integration tests 
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
-insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
+insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Andrew')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'kowalski@domain.com', 'Jan', 'Kowalski')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'REMOVED', 'delete@domain.com', 'Stefan', 'Kowalski')
